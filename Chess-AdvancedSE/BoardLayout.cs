96016f7fe0a02d65dd0fb375f7a88f7ff012b7d1
David Schmidt <mail.schmidtd@gmail.com>
2021-11-15 14:27:39 +0100

    Added BoardLayout

---
+++ Chess-AdvancedSE/BoardLayout.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_AdvancedSE
+{
+    public class BoardLayout: INotifyPropertyChanged
+    {
+        private List<List<Square>> _layout;
+        private List<List<Square>> Layout
+        {
+            get => _layout;
+            set
+            {
+                _layout = value;
+                NotifyPropertyChanged(nameof(Layout));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
