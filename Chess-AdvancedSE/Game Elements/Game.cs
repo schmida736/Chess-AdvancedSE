faa5dbc9608a17dbd591054098f01e6b18edd641
Purdbull <72924017+Purdbull@users.noreply.github.com>
2022-04-17 19:59:19 +0200

    added "player white starts the game"

--- Chess-AdvancedSE/Game Elements/Game.cs
+++ Chess-AdvancedSE/Game Elements/Game.cs
@@ -18,7 +18,7 @@
             playerOne = player;
             playerTwo = new(!player.Color);
 
-            currentPlayerColor = !playerOne.Color;
+            currentPlayerColor = true;
         }
 
         public bool RecieveMoveRequest(int from_row, int from_col, int to_row, int to_col)
