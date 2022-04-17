f56298d753a1cdd66bc40748add0c7f33fce36cb
Purdbull <72924017+Purdbull@users.noreply.github.com>
2022-04-17 19:57:47 +0200

    added player switch between moves

--- Chess-AdvancedSE/Game Elements/Game.cs
+++ Chess-AdvancedSE/Game Elements/Game.cs
@@ -5,16 +5,37 @@
         public Player player;
         public Board board;
 
+        private Player playerOne;
+        private Player playerTwo;
+        private bool currentPlayerColor;
+
+
         public Game()
         {
             player = new(Constants.Color.Random());
             board = new(player);
+
+            playerOne = player;
+            playerTwo = new(!player.Color);
+
+            currentPlayerColor = !playerOne.Color;
         }
 
         public bool RecieveMoveRequest(int from_row, int from_col, int to_row, int to_col)
         {
             System.Console.WriteLine("Move detected");
-            return board.MovePiece(from_row, from_col, to_row, to_col);
+
+            if(board.viewModel.layout[from_row][from_col].Piece?.Color == currentPlayerColor)
+            {
+                if(board.MovePiece(from_row, from_col, to_row, to_col))
+                {
+                    currentPlayerColor = !currentPlayerColor;
+                    return true;
+                }
+            }
+            return false;
         }
+
+        
     }
 }
